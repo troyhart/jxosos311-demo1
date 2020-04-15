3c39671691e5be21f448fed917aa398d1454a6e0
Troy Hart <thart@myriad.com>
2020-04-15 15:50:01 -0600

    Added support for recording name.

--- src/main/java/com/example/jxosos311demo1/HelloController.java
+++ src/main/java/com/example/jxosos311demo1/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.jxosos311demo1;
 
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HelloController {
+    private String name;
+
+    @PutMapping("/name/{name}")
+    public void stateYourName(@PathVariable String name) {
+        Assert.hasText(name, "null/blank name");
+        this.name = name;
+    }
+
     @GetMapping
     public String hello() {
-        return "Hello!";
+        return "Hello " + name == null ? "kind sir!" : name;
     }
 }
