e3f2441c9ce041c888f3cb69e0c09cb74e14c391
Troy Hart <thart@myriad.com>
2020-04-15 12:30:15 -0600

    Initial import

---
+++ src/test/java/com/example/jxosos311demo1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jxosos311demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
