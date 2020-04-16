28378e0c2bd621e112834653430deecf115ef0fd
Troy Hart <thart@myriad.com>
2020-04-16 11:03:51 -0600

    fixed docs

--- src/main/java/com/example/jxosos311demo1/HelloController.java
+++ src/main/java/com/example/jxosos311demo1/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Say hello to the last person to state their name.
+ * Say hello to the last person who has stated their name.
  */
 @RestController
 @RequestMapping("/")
