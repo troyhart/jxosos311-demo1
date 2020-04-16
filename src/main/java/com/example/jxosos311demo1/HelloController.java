e8d0da2bca9e8d9f0ce580022e13420acc35bf10
Troy Hart <thart@myriad.com>
2020-04-16 11:58:53 -0600

    Added logging to controller endpoint methods

--- src/main/java/com/example/jxosos311demo1/HelloController.java
+++ src/main/java/com/example/jxosos311demo1/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.jxosos311demo1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,17 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+    
     private String name;
 
     @PutMapping("/names/{name}")
     public void stateYourName(@PathVariable String name) {
         Assert.hasText(name, "null/blank name");
+        LOGGER.info("Setting name to " + name);
         this.name = name;
     }
 
     @GetMapping
     public String hello() {
         String greeting = "Hello " + (name == null ? "kind sir!" : name);
+        LOGGER.info("Sending greeting: " + greeting);
         return greeting;
     }
 }
