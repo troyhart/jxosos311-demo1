f1302da83dac865ef0d8205526b9dc2e23bbf0b8
Troy Hart <thart@myriad.com>
2020-04-18 18:49:53 -0600

    Added support for history.

--- src/main/java/com/example/jxosos311demo1/HelloController.java
+++ src/main/java/com/example/jxosos311demo1/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.jxosos311demo1;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,13 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
-    
+
     private String name;
+    private LinkedList<String> history = new LinkedList<>();
 
     @PutMapping("/names/{name}")
     public void stateYourName(@PathVariable String name) {
         Assert.hasText(name, "null/blank name");
+        name = name.trim();
         LOGGER.info("Setting name to " + name);
+        if (this.name != null) {
+            history.addFirst(this.name);
+        }
         this.name = name;
     }
 
@@ -36,5 +46,19 @@ public class HelloController {
     @GetMapping("/name")
     public String name() {
         return name;
+    }
+
+    /**
+     * @param count optional count for the maximum number of names to return.
+     * @return the history of stated names. All names by default, but when provided, count will limit the results the the last n,
+     *  where n is equal to the given count.
+     */
+    @GetMapping("/names")
+    public List<String> history(@RequestParam(required = false, defaultValue = "-1") int count) {
+        if (count > 0 && count < history.size()) {
+            return history.stream().limit(count).collect(Collectors.toList());
+        }
+
+        return history;
     }
 }
